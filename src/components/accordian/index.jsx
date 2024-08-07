eceb4d00835d8242275145b9b0a5371735b60196
adiborse111 <adi.borse111@gmail.com>
2024-08-07 09:04:55 +0530

    new commit

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import data from './data';
 
 
 export default function Accordian(){
 
+    const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId){
+        setSelected(getCurrentId);
+    }
+
     return(
-        <div>Accordian</div>
+        <div className="wrapper">
+            <div className="accordian">
+                {
+                    data && data.length > 0 ?(
+                    data.map(dataItem => <div className="item">
+                        <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
+                            <h3>{dataItem.question}</h3>  
+                            <span>+</span>    
+                        </div>
+                        {
+                            selected === dataItem.id ?  
+                            <div></div>
+                            : null
+                        }    
+                    </div>))
+                    : <div>No Data Found !</div>
+                }
+            </div>
+        </div>
     );
 }
