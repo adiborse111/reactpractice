b2766e93dd08c049c123eb7b091ccd6973c8a385
adiborse111 <adi.borse111@gmail.com>
2024-08-08 13:14:45 +0530

    image-slider done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 // eslint-disable-next-line
 import Accordian from './components/accordian'; 
+import ImageSlider from './components/image-slider';
 // eslint-disable-next-line
 import RandomColor from './components/random-color';
+// eslint-disable-next-line
 import StartRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      {/* {<Accordian />} */}
+      {/* { <Accordian /> } */}
       {/* <RandomColor /> */}
-      {<StartRating noOfStars={10} />}
+      {/* {<StartRating noOfStars={10} />} */}
+      {<ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'}/>}
     </div>
   );
 }
