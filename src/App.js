6008d6bb1801127c727f2dfa7f96e723422388c3
adiborse111 <adi.borse111@gmail.com>
2024-08-08 10:56:43 +0530

    star rating done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 // eslint-disable-next-line
 import Accordian from './components/accordian'; 
+// eslint-disable-next-line
 import RandomColor from './components/random-color';
+import StartRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      <RandomColor />
+      {/* {<Accordian />} */}
+      {/* <RandomColor /> */}
+      {<StartRating noOfStars={10} />}
     </div>
   );
 }
